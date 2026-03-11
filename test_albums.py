983b70a27f85bf436213b11355cc0f1b1082d200
Jarle <jarle@he09>
2026-03-11 00:14:41 +0000

    Remove debug test file

--- test_albums.py
+++
@@ -1,7 +0,0 @@
-import flickr_client
-import main
-flickr = flickr_client.get_api()
-user_id = main.resolve_user_id()
-print(f"user_id: {repr(user_id)}")
-resp = flickr_client._api_call(flickr.photosets.getList, user_id=user_id, page=1, per_page=1)
-print(resp)
